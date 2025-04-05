f0122141b315bacfcac0afdc0af1653324b9a128
EepySoldier <szymon.tokarz2@gmail.com>
2025-04-05 20:34:08 +0200

    Deploy works

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
-import redisClient from './configDB';
-import constants from './constants';
+import redisClient from './configDB.js';
+import constants from './constants.js';
 
 const app = express();
 const server = http.createServer(app);
