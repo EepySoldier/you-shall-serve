b81f680f3ddf002cf78d709280f207c365f0b932
EepySoldier <szymon.tokarz2@gmail.com>
2025-04-05 22:58:32 +0200

    Fixed typescript

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
-import redisClient from './configDB.js';
-import constants from './constants.js';
+import redisClient from './configDB.ts';
+import constants from './constants.ts';
 
 const app = express();
 const server = http.createServer(app);
